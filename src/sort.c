10ad0fc686d77424bae29235b640ab822d28dbf7
Adrien Audebert <aaudeber@student.42perpignan.fr>
2023-05-29 18:08:46 +0200

    radix sort with bit len limit

--- src/sort.c
+++ src/sort.c
@@ -6,30 +6,54 @@
 /*   By: aaudeber <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 14:02:42 by aaudeber          #+#    #+#             */
-/*   Updated: 2023/05/29 14:10:47 by aaudeber         ###   ########.fr       */
+/*   Updated: 2023/05/29 18:06:55 by aaudeber         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	ft_find_bit_max(t_list *lst)
+{
+	int	bit_len;
+	int	i;
+	int	mask;
+
+	bit_len = 1;
+	mask = 1;
+	while (lst)
+	{
+		i = 0;
+		while (i < 32)
+		{
+			if (mask << i & lst->nb)
+			{
+				if (bit_len < i)
+					bit_len = i;
+			}
+			i++;
+		}
+		lst = lst->next;
+	}
+	return (bit_len);
+}
 
 void	ft_sort(t_list **lst_a, t_list **lst_b)
 {
 	int	i;
 	int	j;
 	int	len;
+	int	bit_len;
 	unsigned int	mask;
 	t_list	*s1;
 	t_list	*s2;
-	t_list	*tmp;
 
 	i = 0;
 	j = 0;
 	mask = 1;
-	tmp = *lst_a;
 	len = ft_lstsize(*lst_a);
-	ft_putnbr_fd(len, 1);
-	printf("\n");
-	while (j < 8)
+	bit_len = ft_find_bit_max(*lst_a);
+	printf("==%d==\n", bit_len);
+	while (j <= bit_len)
 	{
 		while (i < len)
 		{
